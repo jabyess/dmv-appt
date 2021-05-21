66895cb484ec1c3b39fe74d23745ef388b03c18b
Jimmy Byess <jimmy@skelios.com>
2021-05-21 15:36:54 -0400

    Rework logic to cover all services

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # DMV appointments
 
-## setup
 
-* clone this repo
+## Setup
+
+Node 14 or greater is required
+
+* Clone this repo
 * `npm install`
 * `node request.js`
 * Go make your appointment online at `https://nysdmvqw.us.qmatic.cloud/naoa/index.jsp`
--- request.js
+++ request.js
@@ -1,42 +1,113 @@
 import axios from "axios"
 import rls from "readline-sync"
+import fs from 'fs'
+import { access } from 'fs/promises'
 import {
   services,
   locById,
   serviceToLocMap,
+  locations
 } from "./ids.js"
 
+const BASE_URL = `https://nysdmvqw.us.qmatic.cloud/qwebbook/rest/schedule`
+const MASTER_SERVICES_FILE = './masterServicesList.json'
+
 const promptUser = () => {
-  let svcs = Object.keys(services)
-  let choice = rls.keyInSelect(svcs, "What do you want to search for?")
-  let svc = svcs[choice]
-  if(choice !== -1) {
-    console.log(`Searching for appointment dates for: ${svc}`)
-    getAllDates(serviceToLocMap[svc], svc)
+  // get services and save to disk if they don't exist
+  if(_getAllServicesByLocation()) {
+    let allServices = JSON.parse(fs.readFileSync(MASTER_SERVICES_FILE))
+    let svcs = Object.keys(allServices)
+    console.log(svcs)
   }
   else {
-    console.log("You canceled, goodbye")
+    if(rls.keyInYN("Services file does not exist. Retrieve?")) {
+      _fetchServicesByLocation()
+    }
   }
+  // const svcs = Object.keys(services)
+  // svcs.push("Update Service locations JSON")
+  // prompt user to select a service
+  // const choice = rls.keyInSelect(svcs, "What do you want to search for?")
+  // const svc = svcs[choice]
+
+  // if(choice !== -1) {
+  //   console.log(`Searching for appointment dates for: ${svc}`)
+  //   getAllDates(serviceToLocMap[svc], svc)
+  // }
+  // else {
+  //   console.log("You canceled, goodbye")
+  // }
 }
 
-const getAllServices = async (branch) => {
-  let date = new Date().getTime()
-  let url = `https://nysdmvqw.us.qmatic.cloud/qwebbook/rest/schedule/branches/${branch}/services?_=${date}`
-
+const _getAllServicesByLocation = async () => {
   try {
-    let services = await axios.get(url)
-    console.log(services)
-    // return
-  } catch (err) {
-    console.error(err)
+    await access(MASTER_SERVICES_FILE)
   }
+  catch(err) {
+    return false
+  }
+
+  return true
 }
 
+const _fetchServicesByLocation = async () => {
+  let allServicesAtLocation = {}
+
+  console.log("Fetching...")
+
+  for(let loc in locations) {
+    try {
+      const servicesAtLocation = await axios.get(svcBuilder(locations[loc]))
+      const flattenedServices = servicesAtLocation.data.map(({name, publicId}) => ({name, publicId}))
+      allServicesAtLocation[loc] = flattenedServices
+    }
+    catch(err) {
+      console.error("Error fetching services by location", err)
+    }
+  }
+
+  _groupServicesByLocation(allServicesAtLocation)
+}
+
+const _groupServicesByLocation = (allServicesAtLocation) => {
+  const masterServicesList = {}
+
+  console.log("Building services file and writing to disk...")
+
+  for(let loc in allServicesAtLocation) {
+    const val = allServicesAtLocation[loc]
+    val.forEach(({ name, publicId })=> {
+      if(!masterServicesList?.[name]?.locations) {
+        masterServicesList[name] = {}
+        masterServicesList[name].locations = []
+        masterServicesList[name].locations.push({ name: loc, locId: locations[loc] })
+        masterServicesList[name].serviceId = publicId
+      }
+      else {
+        masterServicesList[name].locations.push({ name: loc, locId: locations[loc] })
+        masterServicesList[name].serviceId = publicId
+      }
+    })
+  }
+
+  fs.writeFileSync(MASTER_SERVICES_FILE, JSON.stringify(masterServicesList))
+  console.log("Services file updated.")
+
+}
+
+// get all services at a given location
+const svcBuilder = (locID) => {
+  let date = new Date().getTime()
+  return `${BASE_URL}/branches/${locID}/services?_=${date}`
+}
+
+// get all services
+const _svcByLocation = (locID) => {}
+
+// get all available dates for a given service at a given location
 const urlbuilder = (locID, svcID) => {
   let date = new Date().getTime()
-  let url = `https://nysdmvqw.us.qmatic.cloud/qwebbook/rest/schedule/branches/${locID}/services/${svcID}/dates?_=${date}`
-
-  return url
+  return `https://nysdmvqw.us.qmatic.cloud/qwebbook/rest/schedule/branches/${locID}/services/${svcID}/dates?_=${date}`
 }
 
 const printDates = (dates) => {
